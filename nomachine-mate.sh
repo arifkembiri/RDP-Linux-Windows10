2be8d8d6485e06acc8885a69417349ed4e03dbc6
arifkembiri <110958613+arifkembiri@users.noreply.github.com>
2024-05-03 21:23:29 +0700

    Update nomachine-mate.sh

--- nomachine-mate.sh
+++ nomachine-mate.sh
@@ -1,4 +1,4 @@
-wget -O ng.sh https://github.com/arifkembiri/RDP-linux-windows10/raw/main/ngrok.sh > /dev/null 2>&1
+wget -O ng.sh https://github.com/arifkembiri/RDP-linux-windows10/blob/main/ngrok.sh > /dev/null 2>&1
 chmod +x ng.sh
 ./ng.sh
 
